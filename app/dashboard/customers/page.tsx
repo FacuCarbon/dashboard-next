9781b5fcc26c379a22f3abd36bc831e7f518fb20
Facucarbon <facundocarbon2015@gmail.com>
2025-03-14 09:59:10 -0300

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Customers() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Customers</h1>
+    </div>
+  );
+}
